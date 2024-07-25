5847f6b8701f2b9577caa4b10940525cfec7c852
Achim Mertens <achim_mertens@gmx.de>
2024-07-25 07:09:59 +0200

    chore: repairs create_replies.js

--- create_replies.js
+++ create_replies.js
@@ -2,37 +2,43 @@ const fs = require('fs');
 const allreadyUpvotedJSON = fs.readFileSync('./allreadyUpvoted.json');
 
 // Load the existing JSON file
-let rawdata = fs.readFileSync('results.json');
+let rawdata = fs.readFileSync('results_02.json');
 let results = JSON.parse(rawdata);
 let allreadyUpvoted = JSON.parse(allreadyUpvotedJSON);
 
 // Initialize an array to store the filtered entries
 let replies = [];
 
-// Iterate through each entry in the results
+// Iterieren Sie durch jeden Eintrag in den Ergebnissen
 results.forEach(entry => {
-
-  let permlink = entry.content.permlink;
-  if (!allreadyUpvoted.find((entry) => entry.permlink.split('/').pop() === permlink)) {
+  let permlink = entry.content.permlink.split('/').pop();
+  if (!allreadyUpvoted.find((upvoted) => upvoted.permlink === permlink)) {
 
     // Check if the keyword "CHARY" is present in the result content
-    if (entry.result.includes("CHARY")) {
+    if (entry.secondResult.includes("CHARY")) {
       // Extract the author and permlink
       let author = entry.content.author;
-      
-      // Construct the new permlink
-      let fullPermlink = `https://peakd.com/@${author}/${permlink}`;
 
       // Extract the reply content
-      let reply = entry.result.split('content: ')[1].trim();
-      reply = reply.substring(1, reply.length - 2); // Remove the enclosing single quotes and the comma at the end
+      let reply = entry.secondResult
 
       // Push the filtered entry into the replies array
-      replies.push({
-        "author": author,
-        "permlink": fullPermlink,
-        "Reply": reply
-      });
+      const cleanedSecondResult = entry.secondResult.replace(/!CHARY:\s+/, '!CHARY:');
+    
+      // Extrahieren Sie den !CHARY Score aus dem bereinigten secondResult
+      const charyScoreMatch = cleanedSecondResult.match(/!CHARY:(\d+)/);
+      if (charyScoreMatch) {
+        const charyScore = parseInt(charyScoreMatch[1], 10);
+
+        // Überprüfen Sie, ob der Score größer als 4 ist
+        if (charyScore > 4) {
+          replies.push({
+            "author": author,
+            "permlink": entry.content.permlink,
+            "Reply": cleanedSecondResult
+          });
+        }
+      }
     }
   }
 });
